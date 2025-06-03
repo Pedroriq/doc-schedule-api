d5108d295e5f0fe117848b695f8fbeb3ab1405e8
Pedro Rocha <pedrohrsilveira@hotmail.com>
2025-06-03 13:02:06 -0300

    ci(Env): configure environment variables to deploy

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,11 +7,11 @@ import { Appointment } from "./entity/Appointment"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "db",
-    port: 5432,
-    username: "postgres",
-    password: "postgres",
-    database: "doc-schedule",
+    host: process.env.DB_HOST || 'localhost',
+    port: Number(process.env.DB_PORT) || 5432,
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    database: process.env.DB_NAME || "doc-schedule",
     logging: false,
     entities: [Doctor, Patient, Appointment],
     migrations: [],
