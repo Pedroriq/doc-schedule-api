846721bf61271d8776cec6cdcd1366aac2949e40
Pedro Rocha <pedrohrsilveira@hotmail.com>
2025-06-03 14:12:17 -0300

    fix(CI): Remove nc command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-echo "Waiting database"
-until nc -z $DB_HOST $DB_PORT; do
-  sleep 1
-done
-
 echo "Database ok, running migrations..."
 npm run typeorm migration:run -- -d dist/data-source.js
 
