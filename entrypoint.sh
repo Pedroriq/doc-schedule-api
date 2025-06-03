610c8e4377d0ba18bcada767ccc5994d6ca8c1ae
Pedro Rocha <pedrohrsilveira@hotmail.com>
2025-06-03 15:05:01 -0300

    fix(Shell Script): Change script js for ts file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Database ok, running migrations..."
-npm run typeorm migration:run -- -d dist/data-source.js
+npm run typeorm migration:run -- -d dist/data-source.ts
 
 echo "🚀 Starting app..."
 npm start
